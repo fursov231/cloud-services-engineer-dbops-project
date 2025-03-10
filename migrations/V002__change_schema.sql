833a5021794dc63e33ed1316fa79fa8f9b18680d
fursov231 <fursov231@hotmail.com>
2025-03-11 01:19:52 +0300

    шаг 8 v3

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -1,5 +1,4 @@
 ALTER TABLE product ADD COLUMN price double precision;
-ALTER TABLE product ADD COLUMN price double precision;
 ALTER TABLE product ADD PRIMARY KEY (id);
 ALTER TABLE orders ADD COLUMN date_created date;
 ALTER TABLE orders ADD PRIMARY KEY (id);
